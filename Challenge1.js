95da9b0650ac33169bddd5066fb62a430484e07f
michael nauman <bodhiandmike@gmail.com>
2015-01-27 21:03:58 -0800

    strike through working

--- Challenge1.js
+++ Challenge1.js
@@ -1,12 +1,11 @@
 // mikeNauman
 
 $(document).ready(function() {
+
 	$("input").on("click", function(e) {
 		console.log($("input[type=text]").val());
 
 	$(".even").css("background-color", $("input[type=text]").val());
-
-		//$("tr").attr("class", ".even").css("background-color", $("input[type=text]").val());
 
 	});	
 
--- todo.js
+++ todo.js
@@ -4,12 +4,25 @@ $(document).ready(function() {
 
 	$("form").on("click", function(e) {
 		console.log($("input[type=text]").val());
-	$("body").append('<div>' + '<input type="checkbox"></checkbox>'+ " " + $('input[type=text]').val() + '<input type="checkbox" checked="checked"></checkbox>' + '</div>');
-	//$("input[type=checkbox]").attr("checked","checked");
 
-	console.log($("input").last());
+		$("body").append
+			('<div class="row">' + 
+				'<input type="checkbox"></checkbox>' + 
+				$('input[type=text]').val() +
+				'<input type="checkbox" checked="checked"></checkbox>' + 
+				'<button>my button</button>' + 
+				'hello' +
+			'</div>');
+	});
 
 
-	});	
+	$("body").on("click", "button", function(e) {
+		console.log("this is a test1");
+	});
+
+	$("body").on("click", "input[type='checkbox']", function(e) {
+		$(this).parent().addClass("completed");
+		console.log("this is a test2");
+	});
 
 });
