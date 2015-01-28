938fc94118771447d4f1b3b2ba163daf51048fc3
michael nauman <bodhiandmike@gmail.com>
2015-01-28 07:25:54 -0800

    initial commit challenge 3

---
+++ challenge3.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  	window.addEventListener('keydown', function(event){
+
+  	// how to get the key press?
+	if (event.keyCode === 39) {
+		// empty password error message should be appended to the page 
+		// that says "please enter a password" under 6
+		//
+
+
+	$("form").on("click", function(e) {	}
+		console.log("this is a test1");
+	});
+
+
+
+
+});
